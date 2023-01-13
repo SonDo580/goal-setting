73317a47095930f2d9316532d99acb3cd4628ced
Son Do <dominhson580@gmail.com>
2023-01-13 22:48:25 +0700

    Hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,6 +18,9 @@ const register = asyncHandler(async (req, res) => {
     throw new Error("User already exists");
   }
 
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
   res.json({ message: "Register User" });
 });
 
