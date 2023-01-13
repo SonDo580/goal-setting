cedff374631d36340edfade672860c37fb3ed352
Son Do <dominhson580@gmail.com>
2023-01-13 22:52:24 +0700

    Create a new user and response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,22 @@ const register = asyncHandler(async (req, res) => {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  res.json({ message: "Register User" });
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 const login = asyncHandler(async (req, res) => {
