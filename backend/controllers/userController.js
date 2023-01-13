ec2a261645ea67b3591adcff7da2bc3f367c6092
Son Do <dominhson580@gmail.com>
2023-01-13 22:19:43 +0700

    Add 2 more controller functions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,16 @@ const register = (req, res) => {
   res.json({ message: "Register User" });
 };
 
+const login = (req, res) => {
+  res.json({ message: "Login User" });
+};
+
+const getMe = (req, res) => {
+  res.json({ message: "User data" });
+};
+
 module.exports = {
   register,
+  login,
+  getMe,
 };
