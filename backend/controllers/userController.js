b8795db0d3040971d6e6e6052162d67faecdfe4b
Son Do <dominhson580@gmail.com>
2023-01-13 22:45:43 +0700

    Register user: basic validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,27 @@
-const register = (req, res) => {
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+
+const User = require("../models/userModel");
+
+const register = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
   res.json({ message: "Register User" });
-};
+});
 
-const login = (req, res) => {
+const login = asyncHandler(async (req, res) => {
   res.json({ message: "Login User" });
-};
+});
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data" });
-};
+});
 
 module.exports = {
   register,
