9cfef6dc25f7e66ba0c02f0decc027ae8079baee
Son Do <dominhson580@gmail.com>
2023-01-13 10:19:50 +0700

    Create a server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
