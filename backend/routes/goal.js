f1cda9ffe80894169f65ec8a0bc8931baef01420
Son Do <dominhson580@gmail.com>
2023-01-13 10:35:47 +0700

    Add 3 other routes

--- backend/routes/goal.js
+++ backend/routes/goal.js
@@ -2,7 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "GET goals" });
+  res.status(200).json({ message: "Get goals" });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Set goal" });
+});
+
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
 });
 
 module.exports = router;
