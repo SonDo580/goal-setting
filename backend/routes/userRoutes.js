b8ee0ced326b5eae0c084d84fb902a64ea1a5d82
Son Do <dominhson580@gmail.com>
2023-01-13 22:32:30 +0700

    Add user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/", userController.register);
+router.post("/login", userController.login);
+router.get("/me", userController.getMe);
 
 module.exports = router;
