528e688e70702a43f4ea185ec3d90da2a46d0290
Son Do <dominhson580@gmail.com>
2023-01-13 10:31:48 +0700

    Use goal router

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const app = express();
 
+const goalRouter = require("./routes/goal");
+app.use("/api/goals", goalRouter);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
