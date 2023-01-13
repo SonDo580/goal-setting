d920d0ca1a526bb032dbad4becb78405759e9355
Son Do <dominhson580@gmail.com>
2023-01-13 11:38:54 +0700

    Use async for controller functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,25 +1,25 @@
-const { json } = require("body-parser");
+const asyncHandler = require("express-async-handler");
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text field");
   }
 
   res.status(200).json({ message: "Set goal" });
-};
+});
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
