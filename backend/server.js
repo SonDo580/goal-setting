10e981e494a70dede32570cb17696936b2e05ef9
Son Do <dominhson580@gmail.com>
2023-01-13 11:33:36 +0700

    Use the error middleware

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({ extended: false }));
 const goalRouter = require("./routes/goal");
 app.use("/api/goals", goalRouter);
 
+const { errorHandler } = require("./middlewares/errorMiddleware");
+app.use(errorHandler);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
