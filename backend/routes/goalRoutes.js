565dcaf7ecf460a89eef10e6929177824500359f
Son Do <dominhson580@gmail.com>
2023-01-14 16:01:18 +0700

    Protect all goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const goalController = require("../controllers/goalController");
 
-router.route("/").get(goalController.getGoals).post(goalController.setGoal);
+const goalController = require("../controllers/goalController");
+const { protect } = require("../middlewares/authMiddleware");
+
+router
+  .route("/")
+  .get(protect, goalController.getGoals)
+  .post(protect, goalController.setGoal);
 
 router
   .route("/:id")
-  .put(goalController.updateGoal)
-  .delete(goalController.deleteGoal);
+  .put(protect, goalController.updateGoal)
+  .delete(protect, goalController.deleteGoal);
 
 module.exports = router;
