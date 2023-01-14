67cfd2459c04252e57372c3d47a75783a3c8d257
Son Do <dominhson580@gmail.com>
2023-01-14 15:17:03 +0700

    Implement login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,7 +40,20 @@ const register = asyncHandler(async (req, res) => {
 });
 
 const login = asyncHandler(async (req, res) => {
-  res.json({ message: "Login User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 const getMe = asyncHandler(async (req, res) => {
