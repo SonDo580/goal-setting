33ce659cb74294da3cb68b484284f1f7fd0761ab
Son Do <dominhson580@gmail.com>
2023-01-14 15:49:17 +0700

    Use protect middleware in 1 route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const userController = require("../controllers/userController");
 
+const { protect } = require("../middlewares/authMiddleware");
+
 router.post("/", userController.register);
 router.post("/login", userController.login);
-router.get("/me", userController.getMe);
+router.get("/me", protect, userController.getMe);
 
 module.exports = router;
