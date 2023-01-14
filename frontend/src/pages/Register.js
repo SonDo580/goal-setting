19d935114c7dedc6ae9e987fa052744dc1cd1a0a
Son Do <dominhson580@gmail.com>
2023-01-14 21:54:16 +0700

    Add state

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,5 +1,16 @@
+import { useState, useEffect } from "react";
+
 function Register() {
-  return <div>Register</div>;
+  const { formData, setFormData } = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  return <div></div>;
 }
 
 export default Register;
