d0b1a24c01d9fcbac4d3e113672e30fbbf38fcb7
Son Do <dominhson580@gmail.com>
2023-01-14 21:50:10 +0700

    Include the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="container">
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
