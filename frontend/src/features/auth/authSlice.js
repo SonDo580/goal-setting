c6ccb905cc08ec812ce4404f224ba44465999972
Son Do <dominhson580@gmail.com>
2023-02-03 23:13:20 +0700

    Create thunk register

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 const user = JSON.parse(localStorage.getItem("user"));
 
@@ -9,6 +10,15 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (err) {}
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
