193631690b68d34458486209ac5629b885232332
Son Do <dominhson580@gmail.com>
2023-02-04 11:04:19 +0700

    Reset state

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
-import { register } from "../features/auth/authSlice";
+import { register, reset } from "../features/auth/authSlice";
 
 function Register() {
   const dispatch = useDispatch();
@@ -30,6 +30,8 @@ function Register() {
     if (isSuccess || user) {
       navigate("/");
     }
+
+    dispatch(reset());
   }, [user, isLoading, isError, isSuccess, message, navigate, dispatch]);
 
   const handleChange = (event) => {
