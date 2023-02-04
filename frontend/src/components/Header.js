942dc19090a9fe60b01c79faccf3d96097be54d1
Son Do <dominhson580@gmail.com>
2023-02-04 11:27:18 +0700

    Show 'Log out' for logged-in user

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,34 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">Goal Setter</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt /> Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            <FaUser /> Register
-          </Link>
-        </li>
+        {user ? (
+          <li>
+            <FaSignOutAlt /> Logout
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
