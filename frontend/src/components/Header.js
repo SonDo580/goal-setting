045e04b6a82a371f163aea13108f998c978e4566
Son Do <dominhson580@gmail.com>
2023-02-04 11:31:56 +0700

    Handle logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,19 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { user } = useSelector((state) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
 
   return (
     <header className="header">
@@ -13,7 +23,9 @@ function Header() {
       <ul>
         {user ? (
           <li>
-            <FaSignOutAlt /> Logout
+            <button className="btn" onClick={handleLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
           </li>
         ) : (
           <>
