51ab4083afd78a5248a03ab0270f610fd0cb1a35
Son Do <dominhson580@gmail.com>
2023-02-04 10:50:24 +0700

    Set up react-toastify

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
@@ -7,16 +9,21 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+      <ToastContainer />
+    </>
   );
 }
 
