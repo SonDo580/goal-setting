de6a76b2afc3863c51399fe6cf1aa297d3ca0f20
Son Do <dominhson580@gmail.com>
2023-02-04 11:52:45 +0700

    Create login service

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 
-const API_URL = "/api/users/";
+const API_URL = "/api/users";
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
+
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+const login = async (userData) => {
+  const response = await axios.post(`${API_URL}/login`, userData);
 
   if (response.data) {
     localStorage.setItem("user", JSON.stringify(response.data));
@@ -18,6 +28,7 @@ const logout = async () => {
 
 const authService = {
   register,
+  login,
   logout,
 };
 
