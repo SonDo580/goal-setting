b446cf88fd6a364d02a2dacc3332b0e5e272a9cf
Son Do <dominhson580@gmail.com>
2023-02-05 17:37:09 +0700

    Serve frontend in production build

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
@@ -11,6 +12,17 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    );
+  });
+}
 
 const { errorHandler } = require("./middlewares/errorMiddleware");
 app.use(errorHandler);
