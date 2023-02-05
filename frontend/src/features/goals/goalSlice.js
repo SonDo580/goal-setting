49e3283a10abac24b648234e910e2cc43ed32f20
Son Do <dominhson580@gmail.com>
2023-02-05 11:02:14 +0700

    Add reducers for createGoal

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -15,6 +15,24 @@ const goalSlice = createSlice({
   reducers: {
     reset: (state) => initialState,
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+
+        state.goals.push(action.payload);
+      })
+      .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+
+        state.message = action.payload;
+      });
+  },
 });
 
 export const createGoal = createAsyncThunk(
