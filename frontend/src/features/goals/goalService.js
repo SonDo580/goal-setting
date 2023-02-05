2bd14cc29425772c06b1adf65c08f02533f34728
Son Do <dominhson580@gmail.com>
2023-02-05 11:51:42 +0700

    Add getGoals service

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -12,8 +12,19 @@ const createGoal = async (goalData, token) => {
   return response.data;
 };
 
+const getGoals = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const response = await axios.get(API_URL, config);
+  return response.data;
+};
+
 const goalService = {
   createGoal,
+  getGoals,
 };
 
 export default goalService;
