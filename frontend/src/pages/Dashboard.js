4a8a0d63459519ac6e959a93e42246a4005c1218
Son Do <dominhson580@gmail.com>
2023-02-05 10:04:14 +0700

    Define structure

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import GoalForm from "../components/GoalForm";
 
 function Dashboard() {
   const navigate = useNavigate();
@@ -13,7 +14,16 @@ function Dashboard() {
     }
   }, [navigate, user]);
 
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goal Dashboard</p>
+
+        <GoalForm />
+      </section>
+    </>
+  );
 }
 
 export default Dashboard;
