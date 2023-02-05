0ba509024112b56ccd363bd76647771579a577c1
Son Do <dominhson580@gmail.com>
2023-02-05 10:09:50 +0700

    Create the form and handle value changes

--- frontend/src/components/GoalForm.js
+++ frontend/src/components/GoalForm.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 function GoalForm() {
-  return <div>GoalForm</div>;
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Goal</label>
+          <input type="text" name="text" value={text} onChange={handleChange} />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block">Add Goal</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default GoalForm;
