d67a1588356269217fefc796e1501c2f7cd8af09
Son Do <dominhson580@gmail.com>
2023-02-05 14:46:56 +0700

    Dispatch action getGoals

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,18 +1,24 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import GoalForm from "../components/GoalForm";
+import { getGoals } from "../features/goals/goalSlice";
 
 function Dashboard() {
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals } = useSelector((state) => state.goals);
 
   useEffect(() => {
     if (!user) {
       navigate("/login");
+    } else {
+      dispatch(getGoals());
     }
-  }, [navigate, user]);
+  }, [dispatch, navigate, user, goals]);
 
   return (
     <>
